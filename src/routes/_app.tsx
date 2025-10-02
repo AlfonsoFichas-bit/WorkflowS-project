14db43be37a68cb2a25d57b6aff8863e2c28b237
AlfonsoFichas-bit <alfonsofichas@yahoo.com>
2025-10-02 15:31:19 -0400

    [FIX] Landing

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -18,7 +18,7 @@ export default define.page(function App({ Component }) {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin=""
+          crossOrigin="anonymous"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700;800&display=swap"
